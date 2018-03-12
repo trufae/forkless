84f3fdfa848808dc7a75be8eeaf30a62ec1713fd
pancake <pancake@nopcode.org>
2018-03-12 01:36:52 +0100

    Improve message and clean more code

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 
 int main() {
 	int i;
-	setjmp(env);
-	size_t child = forkless_fork();
+	setjmp (env);
+	size_t child = forkless_fork ();
 	printf ("== CHILD %p\n", (void*)(size_t)child);
 	if (child == -1) {
 		fprintf (stderr, "Cannot fork\n");
@@ -15,19 +15,12 @@ int main() {
 	if (child) {
 		int rc = forkless_waitpid (child, NULL, 0);
 		printf ("RC=%d\n", rc);
-		//exit (1);
 	} else {
-#if 0
-		for (i = 0; i< 3; i++) {
-			printf ("... %d\n", i);
-			sleep(1);
-		}
-		// char *const args[] = { "ls", "-l", "/bin", NULL };
-#endif
 		char *const args[] = { "echo", "this", "is", "miau", "camole", NULL };
 		int rc = forkless_execve ("echo", args, NULL);
-		printf ("Child is done\n");
-		forkless_exit(rc);
+		// only reachs this point if execution fails
+		printf ("Cannot find execuntable %d\n", rc);
+		forkless_exit (rc);
 	}
 	return 0;
 }
